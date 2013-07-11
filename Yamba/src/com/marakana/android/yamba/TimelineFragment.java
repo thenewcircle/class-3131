ffb9d39958170bbef2f9f89acd268e3e52bf4e4e
G. Blake Meike <blake.meike@gmail.com>
2013-07-11 16:01:46 -0700

    Convert timeline view to fragment

---
+++ Yamba/src/com/marakana/android/yamba/TimelineFragment.java
@@ -0,0 +1,113 @@
+/* $Id: $
+   Copyright 2013, G. Blake Meike
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package com.marakana.android.yamba;
+
+import android.app.ListFragment;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+
+/**
+ *
+ * @version $Revision: $
+ * @author <a href="mailto:blake.meike@gmail.com">G. Blake Meike</a>
+ */
+public class TimelineFragment extends ListFragment implements LoaderCallbacks<Cursor> {
+    private static final int LOADER_ID = 57;
+
+    private static final String[] PROJ = {
+        YambaContract.Timeline.Columns.ID,
+        YambaContract.Timeline.Columns.CREATED_AT,
+        YambaContract.Timeline.Columns.USER,
+        YambaContract.Timeline.Columns.STATUS
+    };
+
+    private static final String[] FROM = new String[PROJ.length - 1];
+    static { System.arraycopy(PROJ, 1, FROM, 0, FROM.length); }
+
+    private static final int[] TO = new int[] {
+        R.id.timeline_timestamp,
+        R.id.timeline_user,
+        R.id.timeline_status
+    };
+
+    static class TimelineBinder implements SimpleCursorAdapter.ViewBinder {
+
+        @Override
+        public boolean setViewValue(View v, Cursor c, int col) {
+            if (R.id.timeline_timestamp != v.getId()) { return false; }
+
+            CharSequence s = "long ago";
+            long t = c.getLong(col);
+            if (0 < t) { s = DateUtils.getRelativeTimeSpanString(t); }
+            ((TextView) v).setText(s);
+            return true;
+        }
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+        return new CursorLoader(
+                getActivity(),
+                YambaContract.Timeline.URI,
+                PROJ,
+                null,
+                null,
+                YambaContract.Timeline.Columns.CREATED_AT + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+        ((SimpleCursorAdapter) getListAdapter()).swapCursor(c);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        ((SimpleCursorAdapter) getListAdapter()).swapCursor(null);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle state) {
+        View v = super.onCreateView(inflater, parent, state);
+
+        //View v = inflater.inflate(R.layout.activity_status, parent, false);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.timeline_row,
+                null,
+                FROM,
+                TO,
+                0);
+        adapter.setViewBinder(new TimelineBinder());
+        setListAdapter(adapter);
+
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+
+        return v;
+    }
+}
